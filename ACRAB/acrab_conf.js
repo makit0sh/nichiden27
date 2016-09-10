32ef9886fed71f3f6e34aef11919f27eb81413e2
kn1cht <ito.turukasiwa@gmail.com>
2016-09-10 22:52:56 +0900

    Add config files

---
+++ ACRAB/acrab_conf.js
@@ -0,0 +1,13 @@
+/****************************************
+* acrab_conf.js
+* modified at 2016-09-10 19:35 by K_Ito
+* config file of acrab.
+* [TO USERS] DO NOT change directly this file.
+* Use setting.html instead.
+****************************************/
+
+// ip addresses of ESP8266
+var ip = ['http://192.168.0.199/', 'http://192.168.0.200/'];
+
+// name of constellations
+var name = ['LED1', 'LED2'];
--- ACRAB/acrab_main.js
+++ ACRAB/acrab_main.js
@@ -1,37 +1,46 @@
-var ip = ['http://192.168.0.199/', 'http://192.168.0.200/'];
-var name = ['LED1', 'LED2'];
+var ip, constellation = [];
 
 function pageInit(){
-  ip.forEach(function(value){GETRequest(value + 'gpio/0');});
+  xhr = new XMLHttpRequest();
+  xhr.addEventListener('loadend', function(){ // xhr.readyStateが4になったら実行される
+    if(xhr.status === 200){
+      var conf = JSON.parse(xhr.response);
+      ip = conf.ip;
+      constellation = Object.keys(conf.constellation);
+      ip.forEach(function(value){GETRequest(value + 'gpio/0');}); // 点灯状況を読み込む
+    }
+    else console.error(xhr.status+' '+xhr.statusText);
+  });
+  xhr.open("GET", "./acrab_conf.json", true); // 設定ファイルを読み込む
+  xhr.send();
 }
 
 function GETAddress(obj, address){
   if(obj.classList.contains("on")){
-    address += 'gpio/0/';
+    address += 'gpio/0';
   }
   else{
-    address += 'gpio/1/';
+    address += 'gpio/1';
   }
   return address;
 }
 
 function GETRequest(address){
-  var xhr= new XMLHttpRequest();
+  var xhr= new XMLHttpRequest(); // XMLHTTPRequestのインスタンス
   xhr.addEventListener('loadend', function(){ // xhr.readyStateが4になったら実行される
     if(xhr.status === 200){
       console.log(xhr.response);
-      checkStatus(JSON.parse(xhr.response));
+      checkStatus(JSON.parse(xhr.response)); // checkStatusでボタンのスタイル変更
     }
-    else{
-      console.error(xhr.status+' '+xhr.statusText);
-    }
+    else console.error(xhr.status+' '+xhr.statusText);
   });
+  xhr.timeout = 5000; // 5秒でタイムアウト
   xhr.open("GET", address);
   xhr.send();
 }
 
 function checkStatus(stat){
-  ['LED1', 'LED2'].forEach(function(value){
+  constellation.forEach(function(value){
     switch(stat[value]){
       case 1:
         console.log(value + " is on");
