fffe03278f36d2edf4b6e53300133a8975d7238f
kn1cht <ito.turukasiwa@gmail.com>
2016-09-19 19:26:13 +0900

    Implemented request from constellation group and checker of their status

--- ACRAB/acrab_main.js
+++ ACRAB/acrab_main.js
@@ -1,28 +1,38 @@
-var ip, port = {};
+var ip, port, group = {};
 
 function pageInit(){ // 読み込み時実行
   /*** 設定ファイル読み込み ***/
   xhr = new XMLHttpRequest();
   xhr.addEventListener('loadend', function(){ // xhr.readyStateが4になったら実行される
     if(xhr.status === 200){
-      var conf = JSON.parse(xhr.response);
-      ip = conf.ip;
-      port = conf.port;
+      var res = JSON.parse(xhr.response);
+      ip = res.ip;
+      port = res.port;
+      group = res.group;
+      buttonInit(); // ボタンに内容を表示する
       pinSettingSend(); // pin設定
     }else console.error(xhr.status+' '+xhr.statusText);
   });
-  xhr.open("GET", "./acrab_conf.json", true); // 設定ファイルを読み込むリクエスト
+  xhr.open('GET', './acrab_conf.json', true); // 設定ファイルを読み込むリクエスト
   xhr.send();
+}
+
+function buttonInit(){
+  Object.keys(port).forEach(function(key){ // 星座|投影機ごと
+      if(document.getElementById(key)) document.getElementById(key).innerText = this[key].name;
+  },port);
+  Object.keys(group).forEach(function(key){ // 星座グループごと
+      if(document.getElementById(key)) document.getElementById(key).innerText = this[key].name;
+  },group);
 }
 
 function pinSettingSend(){
   Object.keys(ip).forEach(function(ipKey){ // モジュールごとに初期設定(pinと星座/投影機の対応)を送信
     var address = ip[ipKey] + 'setConstellationName/status.json?';
 
-    Object.keys(port).forEach(function(portKey){ // 星座/投影機ごとにpin番号とか見に行く
+    Object.keys(port).forEach(function(portKey){ // 星座|投影機ごとにpin番号見に行く
       if(this[portKey].box != null && this[portKey].box != ipKey){return;} // 別のBOX管轄の星座は関係ないので捨てる
       address += this[portKey].pin + '=' + portKey + '&'; // 'p** = ***&'を追加
-      document.getElementById(portKey).innerText = this[portKey].name; // ついでにボタンに名前をつける
     },port);
     address = address.substr(0,address.length - 1); // 末尾の'&'を削る
 
@@ -31,10 +41,10 @@ function pinSettingSend(){
   });
 }
 
+function portStat(obj){return '=' + (obj.classList.contains('on') ? 0 : 1);}
+
 function requestFromConstellation(obj){
-  var address = ip[port[obj.id].box] + 'setPort/status.json?' + obj.id + '=';
-  if(obj.classList.contains("on")) address += 0;
-  else address += 1;
+  var address = ip[port[obj.id].box] + 'setPort/status.json?' + obj.id + portStat(obj);
   console.debug(address);
   getRequest(address);
   return;
@@ -42,13 +52,36 @@ function requestFromConstellation(obj){
 
 function requestFromProjector(obj){
   Object.keys(ip).forEach(function(key){
-    var address = ip[key] + 'setPort/status.json?' + obj.id + '=';
-    if(obj.classList.contains("on")) address += 0;
-    else address += 1;
+    var address = ip[key] + 'setPort/status.json?' + obj.id + portStat(obj);
     console.debug(address);
     getRequest(address);
   });
   return;
+}
+
+function requestFromAll(obj){
+  Object.keys(ip).forEach(function(key){
+    var address = ip[key] + (obj.id==='Set' ? 'allSet' : 'allClear') + '/status.json';
+    console.debug(address);
+    getRequest(address);
+  });
+  return;
+}
+
+function requestFromGroup(obj){
+  var address = {} // 北天と南天に分かれることがあるので両方用意する
+  address.N = ip.N + 'setPort/status.json?';
+  address.S = ip.S + 'setPort/status.json?';
+  group[obj.id].value.forEach(function(key){ // 各星座ごとに設定を足していく
+    if(port[key].box.match('N')) address.N += key + portStat(obj) + '&';
+    if(port[key].box.match('S')) address.S += key + portStat(obj) + '&';
+  });
+  Object.keys(address).forEach(function(key){ // 南北それぞれ送信
+    if(this[key].slice(-1) === '?') return; // 設定がなければ送るのやめる
+    this[key] = this[key].substr(0,this[key].length - 1); // 末尾の'&'を削る
+    console.debug(this[key]);
+    getRequest(this[key]);
+  },address);
 }
 
 function getRequest(address){
@@ -61,21 +94,22 @@ function getRequest(address){
     else console.error(xhr.status+' '+xhr.statusText);
   });
   xhr.timeout = 1000; // 1秒でタイムアウト
-  xhr.open("GET", address);
+  xhr.open('GET', address);
   xhr.send();
 }
 
 function checkStatus(stat){
-  Object.keys(port).forEach(function(key){
-    switch(stat[key]){
-      case 1:
-        //console.info(key + " is on");
-        document.getElementById(key).classList.add("on");
-        break;
-      case 0:
-        //console.info(key + " is off");
-        document.getElementById(key).classList.remove("on");
-        break;
-    }
+  Object.keys(port).forEach(function(key){ // 星座|投影機ごと
+    if(stat[key] && document.getElementById(key)) document.getElementById(key).classList.add('on');
+    else if(document.getElementById(key)) document.getElementById(key).classList.remove('on');
+  });
+  Object.keys(group).forEach(function(groupKey){ // 星座グループごと
+    if(!group[groupKey].value) return;
+    var isOn = true;
+    group[groupKey].value.forEach(function(valueKey){ // 星座ごと
+      if(!stat[valueKey]) isOn = false;
+    });
+    if(isOn) document.getElementById(groupKey).classList.add('on');
+    else document.getElementById(groupKey).classList.remove('on');
   });
 }
