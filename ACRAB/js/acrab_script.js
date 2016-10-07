e6d2e9d8e1b7b325e7689adc9a4317e31ff9e76e
kn1cht <ito.turukasiwa@gmail.com>
2016-10-08 01:27:55 +0900

    Added timer for script viewer

--- ACRAB/js/acrab_script.js
+++ ACRAB/js/acrab_script.js
@@ -11,33 +11,49 @@ var info, script = {};
     viewScript('#script_next', 1);
     $('#script_next').addClass('script2').attr('onclick', 'goNext();');
     $('#script_number').html('1/' + script.length);
+
+    $('#count_start').click(function(){
+      starttime = new Date();
+      timer = setInterval(function(){
+        var currenttime = new Date();
+        var passtime = Math.round((currenttime.getTime() - starttime.getTime()) / 1000);
+        var hour     = toDoubleDigits(Math.floor(passtime / 3600));
+        var minute   = toDoubleDigits(Math.floor((passtime - 3600*hour) / 60));
+        var second   = toDoubleDigits((passtime - 3600*hour - 60*minute));
+        $('#script_timecount').html(hour + ':' + minute + ':' + second);
+        return;
+      }, 1000);
+    });
+    $('#count_stop').click(function(){clearInterval(timer)});
   });
 }());
 
+function toDoubleDigits(num){return ('0' + num).slice(-2)}; // sliceで時刻要素の0埋め
+
 function goNext(){
   $.each(['script_prev', 'script_now', 'script_next'], function(){
-    var num = $('#'+this).get(0).className.match(/\d/g).join("") / 1; // 数字だけ取り出して渡す(型変換しないとうまくいかなかった)
+    var num = $('#'+this).get(0).className.match(/\d/g).join('') / 1; // 数字だけ取り出して渡す(型変換しないとうまくいかなかった)
     $('#'+this).removeClass($('#'+this).get(0).className).addClass('script' + (num+1));
-    if(num+1 > script.length) $('#'+this).html('(原稿の最後です)').removeAttr("onclick");
+    if(num+1 > script.length) $('#'+this).html('(原稿の最後です)').removeAttr('onclick');
     else{
       if(this == 'script_now') sendComm(num, 0);
       viewScript('#'+this, num);
     }
   });
-  $('#script_number').html($('#script_now').get(0).className.match(/\d/g).join("") + '/' + script.length);
+  $('#script_number').html($('#script_now').get(0).className.match(/\d/g).join('') + '/' + script.length);
 }
 
 function goPrev(){
   $.each(['script_prev', 'script_now', 'script_next'], function(){
-    var num = $('#'+this).get(0).className.match(/\d/g).join("") / 1; // 数字だけ取り出して渡す(型変換しないとうまくいかなかった)
+    var num = $('#'+this).get(0).className.match(/\d/g).join('') / 1; // 数字だけ取り出して渡す(型変換しないとうまくいかなかった)
     $('#'+this).removeClass($('#'+this).get(0).className).addClass('script' + (num-1));
-    if(num-1 <= 0) $('#'+this).html('(前のシーンが表示されます)').removeAttr("onclick");
+    if(num-1 <= 0) $('#'+this).html('(前のシーンが表示されます)').removeAttr('onclick');
     else{
       if(this == 'script_now') sendComm(num-1, 1);
       viewScript('#'+this, num-2);
     }
   });
-  $('#script_number').html($('#script_now').get(0).className.match(/\d/g).join("") + '/' + script.length);
+  $('#script_number').html($('#script_now').get(0).className.match(/\d/g).join('') + '/' + script.length);
 }
 
 function sendComm(index, reverse){
