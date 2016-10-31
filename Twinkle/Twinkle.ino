613698fdc80df002f27a562679d4bcaf6986ec9d
kn1cht <ito.turukasiwa@gmail.com>
2016-10-31 23:58:00 +0900

    Twinkle class

--- Twinkle/Twinkle.ino
+++ Twinkle/Twinkle.ino
@@ -1,26 +1,27 @@
-/*
- * Twinkle_North_final.c
- *
- * Created: 2012/11/07 18:57:08
- *  Author: Haruki Nishimura(Nichiden_23)
- *Caution!!! ヒューズは　Lo = E2, Hi= DF, Ex = FF　に設定すること！(DIVCLK8をDisableする)
- */
-#define F_CPU 8000000UL
+/*****************************************************************************************
+*                                      Twinkle.ino
+* Created: 2012/11/07 18:57:08
+* Author: Haruki Nishimura(Nichiden_23)
+* Edited: 2016/10/31 12:52:48 by K_Ito(Nichiden_27)
+*
+* Caution!!! ヒューズはLo = E2, Hi= DF, Ex = FFに設定すること！(DIVCLK8をDisableする)
+* K_Ito追記: Arduino IDEに以下のjsonを読み込ませるとボードマネージャからAVRが追加できる。
+*   http://drazzy.com/package_drazzy.com_index.json
+*
+* 制御用変数の説明
+*   twinkle_patterns:またたきのパターン数。
+*   twinkle_rate:またたき用の乱数値の更新レート。これにnを設定するとn回のタイマ割込に1回の割合で値が更新する。（2324プラネでは12個の乱数更新している）
+*   chaos_div:chaos_gen()で生成される乱数は1~32768の幅であるが、このままではタイマ0割込に使用できないので適当な2の乗数で割る。
+*   led_int_max:LEDの点灯時間の最大値を決定するパラメーター。これはタイマ割込の間隔によって決まっているのでオシロスコープで波形を見ながら調整のこと。
+*   min_strong,min_weak:点灯時間の最小値を決定するパラメーター。これが小さいほどまたたきの度合いが強い。
+*   twinkle_shift:乱数の周期性問題を解決するために、乱数とそれに対応する信号出力ビットをport_shift()で変更している。これにnを設定するとn回の乱数更新に1回の割合で出力がビットシフトする。
+*****************************************************************************************/
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-/*
- *制御用変数の説明
- *twinkle_patterns        :またたきのパターン数。
- *twinkle_rate          :またたき用の乱数値の更新レート。これにnを設定するとn回のタイマ割込に1回の割合で値が更新する。（2324プラネでは12個の乱数更新している）
- *chaos_div            :chaos_gen()で生成される乱数は1~32768の幅であるが、このままではタイマ0割込に使用できないので適当な2の乗数で割る。
- *led_int_max          :LEDの点灯時間の最大値を決定するパラメーター。これはタイマ割込の間隔によって決まっているのでオシロスコープで波形を見ながら調整のこと。
- *min_strong,min_weak      :点灯時間の最小値を決定するパラメーター。これが小さいほどまたたきの度合いが強い。
- *twinkle_shift          :乱数の周期性問題を解決するために、乱数とそれに対応する信号出力ビットをport_shift()で変更している。これにnを設定するとn回の乱数更新に1回の割合で出力がビットシフトする。
- */
-
+#define F_CPU 8000000UL
 #define twinkle_patterns 12
 #define twinkle_rate 2
 #define chaos_div 256
@@ -29,13 +30,23 @@
 #define min_weak 90
 #define twinkle_shift 100
 
+extern "C" void INT0_vect(void) __attribute__ ((signal));
+extern "C" void TIMER0_COMPA_vect(void) __attribute__ ((signal));
+
+class Twinkle {
+  private:
+  public:
+    Twinkle();
+    friend void INT0_vect(void);
+    friend void TIMER0_COMPA_vect(void);
+};
+
 unsigned long chaos[12] = {512,844,1020,1024,2020,2048,2424,3624,4824,8224,8884,11024};
 unsigned long shift_chaos = 28000;
 unsigned int led_int[12] = {160,160,160,160,160,160,160,160,160,160,160,160};
 int  bit_num[6] = {0,1,2,3,4,5};
 unsigned int tr = twinkle_rate;
 unsigned int ts = twinkle_shift;
-
 unsigned int left_shift(unsigned int i, unsigned int a)//6個の数字の並んでいる数列を左にaシフトしたとき、右からi番目に並んでいる数を与える。
 {
   i = i + a;
@@ -99,37 +110,13 @@ void chaos_renew(void)//乱数値を更新する。所要時間は12変数で1ms
   return;
 }
 
-void setup(void)//初期設定（TIMER0_CTC、TIMER割込、INT0割込）
-{
-  _delay_ms(100);
-  DDRA  =  0xFF;  //PORTA0~7を出力に設定
-  DDRB  =  0xFF;
-  /*タイマ0
-    CTCモード、タイマ0_Compare_A割込*/
-  TCCR0A  =  0x01;//CTC0をHに
-  TCCR0B  =  0x00
-      |  1<<CS02
-      |  0<<CS01
-      |  0<<CS00;
-  OCR0A  =  180;//CTCのMAX値の設定(180、プリスケーラ256の設定でタイマ割込間隔は7.5msec)
-  TIMSK  |=  1<<OCIE0A//タイマ0CompA割込有効
-      |  0<<TOIE0;
-  GIMSK  |=  1<<INT0;//INT0割り込み有効
-  MCUCR  |=  0<<ISC01
-      |  0<<ISC00;//INTピンのLowで割り込み発生
-  PORTA  =  0x00;
-  PORTB  =  0x00;
-  return;
-}
 
-ISR (INT0_vect)//またたきOFF（スイッチで切り替え）
-{
+void INT0_vect(void){ //またたきOFF（スイッチで切り替え）
   PORTA = 0xFF;
   PORTB = 0xFF;
 }
 
-ISR (TIMER0_COMPA_vect)//またたきをつかさどる部分。PORTAがまたたき強い、PORTBがまたたき弱い。
-{
+void TIMER0_COMPA_vect(void) { //またたきをつかさどる部分。PORTAがまたたき強い、PORTBがまたたき弱い。
   int i;
   unsigned int c_up = 0;
   unsigned int x = twinkle_patterns;
@@ -168,9 +155,27 @@ ISR (TIMER0_COMPA_vect)//またたきをつかさどる部分。PORTAがまた�
   return;
 }
 
-int main(void)
-{
-  setup();
-    sei();
+int main(void){
+  /*** 初期設定(TIMER0_CTC、TIMER割込、INT0割込) ***/
+  _delay_ms(100);
+  DDRA = 0xFF;  //PORTA0~7を出力に設定
+  DDRB = 0xFF;
+  /*タイマ0 CTCモード、タイマ0_Compare_A割込*/
+  TCCR0A = 0x01; //CTC0をHに
+  TCCR0B = 0x00
+         | 1<<CS02
+         | 0<<CS01
+         | 0<<CS00;
+  OCR0A  = 180; //CTCのMAX値の設定(180、プリスケーラ256の設定でタイマ割込間隔は7.5msec)
+  TIMSK |= 1<<OCIE0A //タイマ0CompA割込有効
+        |  0<<TOIE0;
+  GIMSK |= 1<<INT0; //INT0割り込み有効
+  MCUCR |= 0<<ISC01
+        |  0<<ISC00; //INTピンのLowで割り込み発生
+  PORTA  = 0x00;
+  PORTB  = 0x00;
+
+  Twinkle twinkle();
+  sei();
   for(;;){}
 }
