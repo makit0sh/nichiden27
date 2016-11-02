f9b3bd85329a4f5f61b8403f10dedf842863e838
kn1cht <ito.turukasiwa@gmail.com>
2016-11-03 00:18:27 +0900

    Added HC4511 class

--- akutou/AkutouController28/AkutouController28.ino
+++ akutou/AkutouController28/AkutouController28.ino
@@ -8,7 +8,6 @@
 * Powered by bhagman's SoftPWM Library(https://github.com/bhagman/SoftPWM/wiki).
 *
 * Todos
-*   7SEG class -> setup and modeChange
 *   several pattern array(need to change mode_flag)
 *****************************************************************************************/
 
@@ -51,15 +50,30 @@ const int VolumeToPin[4][3] = {\
   {VR5, PWM_W4, PWM_E4},\
 };
 const int FadeTime = 100; // Time in milliseconds of 0 -> 255 or 255 -> 0
-
 const int LightPattern[4][60] = {\
   {255,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13,26,38,51,64,77,90,102,115,128,115,102,90,77,64,51,38,26,13,0,0,0,0,0,0,0,0,0,0,0},\
   {0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13,26,38,51,64,77,90,102,115,128,141,154,166,179,192,205,218,230,243,255,243,230,218,205,192,179,166,154,141,128,115,102,90,77,64,51,38,26,13,0},\
   {0,0,255,0,0,26,51,77,102,128,154,179,205,230,255,230,205,179,154,128,102,77,51,26,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},\
   {0,0,0,255,0,13,26,38,51,64,77,90,102,115,128,125,122,119,116,114,111,108,105,102,99,96,93,90,87,84,82,79,76,73,70,67,64,61,58,55,53,50,47,44,41,38,35,32,29,27,24,21,18,15,12,9,6,3,0,0}
 }; // light controll pattern(max 255)
-
 volatile bool mode_flag = 0;
+
+struct HC4511{
+  int pin[4];
+  HC4511(int p0, int p1, int p2, int p3): pin{p0,p1,p2,p3}{
+    REP(i,4){
+      pinMode(pin[i], OUTPUT);
+      digitalWrite(pin[i], HIGH); // all HIGH for blank
+    }
+  }
+  void write(int value){
+      REP(i,4){
+        digitalWrite(pin[i], value % 2);
+        value /= 2;
+      }
+  }
+  HC4511& operator = (int value){write(value);return *this;}
+};
 
 void modeChange(){mode_flag = !mode_flag;}
 
@@ -67,12 +81,10 @@ void setup(){
   pinMode(INT_SW, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(INT_SW), modeChange, FALLING);
   pinMode(DIR_SW, INPUT_PULLUP);
-  pinMode(SEG0, OUTPUT);
-  pinMode(SEG1, OUTPUT);
-  pinMode(SEG2, OUTPUT);
-  pinMode(SEG3, OUTPUT);
-
-  SoftPWMBegin(); // Initialize SoftPWM library
+  HC4511 segLED(SEG0, SEG1, SEG2, SEG3);
+  segLED = mode_flag;
+  /* Initialize SoftPWM library */
+  SoftPWMBegin();
   REP(i,SIZE_OF(PWM_Pins)){
     SoftPWMSet(PWM_Pins[i], 0);
     SoftPWMSetFadeTime(PWM_Pins[i], FadeTime, FadeTime);
@@ -80,18 +92,19 @@ void setup(){
 }
 
 void loop(){
-  if(mode_flag){
+  segLED = mode_flag;
+  if(!mode_flag){ // Manual mode
+    REP(i,4){
+      int input = analogRead(VolumeToPin[i][0]);
+      SoftPWMSet(VolumeToPin[i][1], map(input, 0, 1023, 0, 255));
+      delayMicroseconds(100);
+    }
+  }
+  else{ // Auto mode
     int input = analogRead(VR1);
     int position = round(SIZE_OF(LightPattern[0]) * input / 1023);
     REP(i,4){
       SoftPWMSet(VolumeToPin[i][1], LightPattern[i][position]);
-      delayMicroseconds(100);
-    }
-  }
-  else{
-    REP(i,4){
-      int input = analogRead(VolumeToPin[i][0]);
-      SoftPWMSet(VolumeToPin[i][1], map(input, 0, 1023, 0, 255));
       delayMicroseconds(100);
     }
   }
