e8c42b089e40804042f6e31e0b2665373ac09708
kn1cht <ito.turukasiwa@gmail.com>
2016-11-04 20:57:34 +0900

    NORTH / SOUTH setting and other minor fix

--- Twinkle/Twinkle.ino
+++ Twinkle/Twinkle.ino
@@ -12,9 +12,9 @@
 *   TWINKLE_RATE:またたき用の乱数値の更新レート。これにnを設定するとn回のタイマ割込に1回の割合で値が更新する。（2324プラネでは12個の乱数更新している）
 *   TWINKLE_SHIFT:乱数の周期性問題を解決するために、乱数とそれに対応する信号出力ビットをport_shift()で変更している。これにnを設定するとn回の乱数更新に1回の割合で出力がビットシフトする。
 *   TWINKLE_PATTERNS:またたきのパターン数。
-*   chaos_div:chaos_gen()で生成される乱数は1~32768の幅であるが、このままではタイマ0割込に使用できないので適当な2の乗数で割る。
-*   on_duration_max:LEDの点灯時間の最大値を決定するパラメーター。これはタイマ割込の間隔によって決まっているのでオシロスコープで波形を見ながら調整のこと。
-*   min_strong,min_weak:点灯時間の最小値を決定するパラメーター。これが小さいほどまたたきの度合いが強い。
+*   CHAOS_DIV:chaos_gen()で生成される乱数は1~32768の幅であるが、このままではタイマ0割込に使用できないので適当な2の乗数で割る。
+*   ON_DURATION_MAX:LEDの点灯時間の最大値を決定するパラメーター。これはタイマ割込の間隔によって決まっているのでオシロスコープで波形を見ながら調整のこと。
+*   MIN_STRONG,MIN_WEAK:点灯時間の最小値を決定するパラメーター。これが小さいほどまたたきの度合いが強い。
 *****************************************************************************************/
 
 #include <avr/io.h>
@@ -23,10 +23,12 @@
 #include <math.h>
 
 #define F_CPU 8000000UL
-#define chaos_div 256
-#define on_duration_max 160
-#define min_strong 30
-#define min_weak 90
+#define NORTH_HEMISPHERE
+//#define SOUTH_HEMISPHERE
+#define CHAOS_DIV 256
+#define ON_DURATION_MAX 160
+#define MIN_STRONG 30
+#define MIN_WEAK 90
 #define TWINKLE_RATE 2
 #define TWINKLE_SHIFT 100
 #define TWINKLE_PATTERNS 12
@@ -35,12 +37,18 @@
 int bit_num[6] = {0,1,2,3,4,5};
 unsigned int on_duration[12] = {160,160,160,160,160,160,160,160,160,160,160,160};
 unsigned long shift_chaos = 28000;
-unsigned long chaos[12] = {512,844,1020,1024,2020,2048,2424,3624,4824,8224,8884,11024};
-                  //south {12404,14884,18200,18402,20442,21488,24242,28004,28824,30224,32008,32024};
+unsigned long chaos[12] =
+#ifdef NORTH_HEMISPHERE
+  {512,844,1020,1024,2020,2048,2424,3624,4824,8224,8884,11024};
+#endif
+#ifdef SOUTH_HEMISPHERE
+  {12404,14884,18200,18402,20442,21488,24242,28004,28824,30224,32008,32024};
+#endif
 unsigned int refresh_rate = TWINKLE_RATE;
 unsigned int rr_count = TWINKLE_RATE;
 unsigned int shift_rate = TWINKLE_SHIFT;
 unsigned int sr_count = TWINKLE_SHIFT;
+
 unsigned long chaos_gen(unsigned long y){ // Max == 32768までの整数値を返す疑似乱数(1/fゆらぎ)
   if(y < 1638) y += 2 * pow(y, 2) / 32768 + 1966;
   else if(y < 16384) y += 2 * pow(y, 2) / 32768;
@@ -57,11 +65,19 @@ void port_shift(void){ // 疑似周期性を目立たなくするために乱数
 void refresh(void){ // 乱数値を更新する。所要時間は12変数で1ms
   for(int i=0;i<12;i++){
     chaos[i] = chaos_gen(chaos[i]);
-    on_duration[i] = min((int)(chaos[i] / chaos_div + i<6 ? min_strong : min_weak), on_duration_max);
+    on_duration[i] = min((int)(chaos[i] / CHAOS_DIV + i<6 ? MIN_STRONG : MIN_WEAK), ON_DURATION_MAX);
   }
 }
 
-void forward_count(void){
+ISR(INT0_vect){ //またたきOFF（スイッチで切り替え）
+  PORTA = 0xFF;
+  PORTB = 0xFF;
+}
+
+ISR(TIMER0_COMPA_vect){ //またたきをつかさどる部分
+  PORTA = 0xFF;
+  PORTB = 0xFF;
+  /* カウントを進める */
   rr_count--; //乱数更新時期の判定と実行をする
   if(!rr_count){
     sr_count--; //ビットシフト更新時期の判定と実行をする
@@ -72,18 +88,9 @@ void forward_count(void){
     refresh();
     rr_count = refresh_rate;
   }else _delay_us(1100); //それらの操作をしない場合でも、同じだけの時間waitして調整する
-}
-ISR(INT0_vect){ //またたきOFF（スイッチで切り替え）
-  PORTA = 0xFF;
-  PORTB = 0xFF;
-}
-
-ISR(TIMER0_COMPA_vect){ //またたきをつかさどる部分。PORTAがまたたき強い、PORTBがまたたき弱い。
+  /* ポートに出力 */
   unsigned int c_up = 0;
   unsigned int pattern_count = TWINKLE_PATTERNS;
-  PORTA = 0xFF;
-  PORTB = 0xFF;
-  forward_count();
   while(pattern_count){ //on_durationの値とc_up（カウントアップ）の値を比較し、一致するまではON、一致したらLEDをOFFにする。全部OFFになったらループを抜けてmainに戻る。
     c_up++;
     for(int i=0;i<12;i++){
