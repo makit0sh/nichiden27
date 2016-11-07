2220e442459543d367139ea4d8061a8893260f50
kn1cht <ito.turukasiwa@gmail.com>
2016-11-07 20:22:05 +0900

    script -> scenario

--- ACRAB/js/acrab_script.js
+++ ACRAB/js/acrab_script.js
@@ -1,27 +1,38 @@
-var info, script = {};
+var info, scenario = {};
+const SCENARIO_COUNT = 2;
 
 (function(){
-  $.when($.getJSON('testscript.json')).done(function(data){
-    info = data.info;
-    script = data.script;
-    scriptInit();
-  });
+  for(var i=0;i<SCENARIO_COUNT;i++){ // Initialize select box
+    $.when($.getJSON('scenario/'+ i +'.json')).done(function(data){
+      info = data.info;
+    });
+  }
+  getScenarioData(0);
+  scenarioInit();
 }());
 
-function scriptInit(){
-  $('#script_title').html('<b>'+info.title+'</b> by'+info.name);
-  $('#script_prev').html('(前のシーンが表示されます)').addClass('script0').attr('onclick', 'goPrev();').prop('disabled', true);
-  viewScript('#script_now', 0);
-  $('#script_now').addClass('script1').prop('disabled', true);
-  viewScript('#script_next', 1);
-  $('#script_next').addClass('script2').attr('onclick', 'goNext();').prop('disabled', true);
-  $('#script_number').html('1/' + script.length);
+
+function getScenarioData(num){
+    $.when($.getJSON('scenario/'+ num +'.json')).done(function(data){
+      info = data.info;
+      scenario = data.scenario;
+    });
+}
+
+function scenarioInit(){
+  $('#scenario_title').html('<b>'+info.title+'</b> by'+info.name);
+  $('#scenario_prev').html('(前のシーンが表示されます)').addClass('scenario0').attr('onclick', 'goPrev();').prop('disabled', true);
+  viewScript('#scenario_now', 0);
+  $('#scenario_now').addClass('scenario1').prop('disabled', true);
+  viewScript('#scenario_next', 1);
+  $('#scenario_next').addClass('scenario2').attr('onclick', 'goNext();').prop('disabled', true);
+  $('#scenario_number').html('1/' + scenario.length);
 }
 
 var timer_button = new function(){
   this.start = function(){
     sendComm(0, 0);
-    $('#script_next').prop('disabled', false);
+    $('#scenario_next').prop('disabled', false);
     timer = setInterval(function(){pass_time++; readTime();}, 1000);
     $('#timer_start').hide();
     $('#timer_stop').show();
@@ -42,7 +53,7 @@ var timer_button = new function(){
   this.reset = function(){
     pass_time = 0;
     readTime();
-    scriptInit();
+    scenarioInit();
     $('#timer_restart').hide();
     $('#timer_start').show();
     $('#timer_reset').prop('disabled', true);
@@ -52,40 +63,40 @@ var timer_button = new function(){
     var hour     = toDoubleDigits(Math.floor(pass_time / 3600));
     var minute   = toDoubleDigits(Math.floor((pass_time - 3600*hour) / 60));
     var second   = toDoubleDigits((pass_time - 3600*hour - 60*minute));
-    $('#script_timecount').html(hour + ':' + minute + ':' + second);
+    $('#scenario_timecount').html(hour + ':' + minute + ':' + second);
     return;
   };
   var toDoubleDigits = function(num){return ('0' + num).slice(-2);}; // sliceで時刻要素の0埋め
 };
 
 function goNext(){
-  $.each(['script_prev', 'script_now', 'script_next'], function(){
+  $.each(['scenario_prev', 'scenario_now', 'scenario_next'], function(){
     var num = $('#'+this).get(0).className.match(/\d/g).join('') / 1; // 数字だけ取り出して渡す(型変換しないとうまくいかなかった)
-    $('#'+this).removeClass($('#'+this).get(0).className).addClass('script' + (num+1));
-    if(num+1 > script.length) $('#'+this).html('(原稿の最後です)').prop('disabled', true);
+    $('#'+this).removeClass($('#'+this).get(0).className).addClass('scenario' + (num+1));
+    if(num+1 > scenario.length) $('#'+this).html('(原稿の最後です)').prop('disabled', true);
     else{
-      if(this == 'script_now') sendComm(num, 0);
+      if(this == 'scenario_now') sendComm(num, 0);
       viewScript('#'+this, num);
     }
   });
-  $('#script_number').html($('#script_now').get(0).className.match(/\d/g).join('') + '/' + script.length);
+  $('#scenario_number').html($('#scenario_now').get(0).className.match(/\d/g).join('') + '/' + scenario.length);
 }
 
 function goPrev(){
-  $.each(['script_prev', 'script_now', 'script_next'], function(){
+  $.each(['scenario_prev', 'scenario_now', 'scenario_next'], function(){
     var num = $('#'+this).get(0).className.match(/\d/g).join('') / 1; // 数字だけ取り出して渡す(型変換しないとうまくいかなかった)
-    $('#'+this).removeClass($('#'+this).get(0).className).addClass('script' + (num-1));
+    $('#'+this).removeClass($('#'+this).get(0).className).addClass('scenario' + (num-1));
     if(num-1 <= 0) $('#'+this).html('(前のシーンが表示されます)').prop('disabled', true);
     else{
-      if(this == 'script_now') sendComm(num-1, 1);
+      if(this == 'scenario_now') sendComm(num-1, 1);
       viewScript('#'+this, num-2);
     }
   });
-  $('#script_number').html($('#script_now').get(0).className.match(/\d/g).join('') + '/' + script.length);
+  $('#scenario_number').html($('#scenario_now').get(0).className.match(/\d/g).join('') + '/' + scenario.length);
 }
 
 function sendComm(index, reverse){
-  var data = $.extend(true, {}, script[index].projector);
+  var data = $.extend(true, {}, scenario[index].projector);
   if(reverse) $.each(data, function(key){
     data[key] = this == 1 ? 0 : 1;
     });
@@ -96,19 +107,19 @@ function sendComm(index, reverse){
 }
 
 function viewScript(id, index){
-  if($(id).is(':disabled'))console.log(id); $(id).prop('disabled', false);
+  if($(id).is(':disabled'))$(id).prop('disabled', false);
   $(id).html(function(){
     var res = ''
-    if(!script[index].word) res += '開始直後'
+    if(!scenario[index].word) res += '開始直後'
     else {
-      res += '「'+script[index].word+'」の';
-      switch(script[index].timing){
+      res += '「'+scenario[index].word+'」の';
+      switch(scenario[index].timing){
         case 'pre': res += '前'; break;
         case 'post': res += '後'; break;
-        default: res +=  script[index].timing; break;
+        default: res +=  scenario[index].timing; break;
       }
     }
-    $.each(script[index].projector, function(key){
+    $.each(scenario[index].projector, function(key){
       res += '<br>' + port[key].name + 'を' + (this == 1 ? '点灯' : '消灯');
     });
     return res;
